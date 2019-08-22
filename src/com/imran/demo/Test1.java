7329b1d7505ccc9c4dc2598712cbc9f04c730c47
highfy life <abdul23imran@gmail.com>
2019-08-22 21:20:31 +0530

    FIRST COMMIT

---
+++ src/com/imran/demo/Test1.java
@@ -0,0 +1,19 @@
+package com.imran.demo;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String hungry="yes";
+	    
+	    if(hungry=="yes") {
+	    	System.out.println("EAT SOMETHING..............");
+	    }
+
+	    else {
+	    	System.out.println("DO YOUR WORK............");
+	    }
+	}
+
+}
